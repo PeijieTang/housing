ee1f9eb6b33b8e80c41c21746592e11becc87cbf
Peijie Tang <149037246+PeijieTang@users.noreply.github.com>
2024-09-23 22:56:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,42 +4,41 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
+st.title('California housing data(1990) ')
+df = pd.read_csv('housing/housing.csv')
 
-st.title('California Housing Data(1990)')
-df = pd.read_csv('housing.csv')
+price_filter = st.slider('Minimal Median House Price', 0, 500001, 200000)
 
-# note that you have to use 0.0 and 40.0 given that the data type of population is float
-price_filter = st.slider('Minimal Median House Price):', 0.0, 50000, 20000)  # min, max, default
-
-# create a multi select
+# Create a multiselect
 location_filter = st.sidebar.multiselect(
-     'Choose the location type',
-     df.ocean_proximityl.unique(),  # options
-     df.ocean_proximity.unique())  # defaults
+    'Choose the location type',
+    df.ocean_proximity.unique(),  # options
+    df.ocean_proximity.unique())  # defaults
 
-# create a input form
-income_level = st.radio(
-    "Select Income Level:",
-    ['Low ', 'Medium', 'High']
+# Create a radio button
+income_level = st.sidebar.radio(
+    'Choose income level:', 
+    ['Low', 'Medium', 'High']
 )
 
-# Filter the DataFrame based on selections
-if income_level == 'Low(<2.5)':
-    filtered_df = df[df['median_income'<= 2.5]]
-elif income_level =='Medium(>2.5 & <4.5)':
-    filtered_df  = df[(df['median_income'] > 2.5) & (df['median_income']< 4.5)]
+if income_level == 'Low':
+    df = df[df['median_income'] <= 2.5]
+elif income_level == 'Medium':
+    df = df[(df['median_income'] > 2.5) & (df['median_income'] < 4.5)]
 else:
-    filtered_df = df[df[df['median_income'> 4.5]]]
+    df = df[df['median_income'] >= 4.5]
 
 df = df[df.median_house_value >= price_filter]
 
+
 df = df[df.ocean_proximity.isin(location_filter)]
 
-if income_level == '(Low<2.5)':
-    filtered_df = df[df['median_income'] <=2.5]
-elif income_level == 'Medium(>2.5 & <4.5)':
-    filtered_df = df[(df['median_income'] > 2.5) & (df['median_income'] < 4.5)]
-else: 
-    filtered_df = df[df['median_income'>4.5]]
-
 st.map(df)
+
+st.subheader('Median House Value')
+fig, ax = plt.subplots(figsize=(20, 10))
+ax.hist(df['median_house_value'], bins=30)
+ax.set_title('Histogram of Median House Value', fontsize=16)
+ax.set_xlabel('Median House Value', fontsize=12)
+ax.set_ylabel('Frequency', fontsize=12)
+st.pyplot(fig)
